881a9b140abbdcffad95db23d8a4951209d1f7a3
trme3 <treymeares@gmail.com>
2021-07-01 14:12:30 -0400

    added edit dialog

--- module6/qt_files/main_window.py
+++ module6/qt_files/main_window.py
@@ -1,7 +1,11 @@
+import os
 import sys
 
 from PyQt5 import uic, QtWidgets
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import QDir
+from PyQt5.QtWidgets import QMessageBox, QFileDialog
+from league_database import LeagueDatabase
+from league import League
 
 Ui_MainWindow, QtBaseWindow = uic.loadUiType("main_window.ui")
 
@@ -10,21 +14,46 @@ class MainWindow(QtBaseWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self._dia_leagues = []
         self.add_league.clicked.connect(self.add_button_clicked)
         self.delete_league.clicked.connect(self.delete_button_clicked)
         self.action_open.triggered.connect(self.action_open_triggered)
 
     def action_open_triggered(self):
-        mb = QMessageBox(QMessageBox.Icon.Critical, "Ouch", "Can't open files", QMessageBox.StandardButton.Ok)
-        mb.exec()
+        dialog = QFileDialog()
+        dialog.setFileMode(QFileDialog.AnyFile)
+        dialog.setFilter(QDir.Files)
+
+        if dialog.exec_():
+            dia_file_name = dialog.selectedFiles()
+
+            if dia_file_name[0].endswith('.pickle'):
+                league_loaded = LeagueDatabase.load(str(dia_file_name[0]))
+                print(league_loaded)
+                for x in league_loaded.leagues:
+                    self._dia_leagues.append(x)
+                    self.update_ui()
 
     def delete_button_clicked(self):
-        dialog = QMessageBox(QMessageBox.Icon.Question, "Are you sure?", "Are you sure you want to delete League",
-                             QMessageBox.StandardButton.Ok | QMessageBox.StandardButton.Cancel)
-        dialog.exec()
+        dialog = QMessageBox(QMessageBox.Icon.Question, "Remove League?", "Are you sure you want to delete League",
+                             QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+        result = dialog.exec()
+        if result == QMessageBox.StandardButton.Yes:
+            del self._dia_leagues[self.league_list.currentRow()]
+            self.update_ui()
+        else:
+            print("Aborted")
 
     def add_button_clicked(self):
+        l = League(self.league_name_add.text(), self.league_oid_add)
+        self._dia_leagues.append(l)
+        self.update_ui()
         self.league_list.addItem(self.league_name_add.text())
+
+    def update_ui(self):
+        self.league_list.clear()
+        for x in self._dia_leagues:
+            self.league_list.addItem(str(x))
 
 
 if __name__ == '__main__':
