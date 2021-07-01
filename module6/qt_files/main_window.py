1ee039a32940665586b05973946fe90d3d752425
trme3 <treymeares@gmail.com>
2021-07-01 12:03:43 -0400

    added delete leage dialogue

--- module6/qt_files/main_window.py
+++ module6/qt_files/main_window.py
@@ -1,6 +1,7 @@
 import sys
 
 from PyQt5 import uic, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 
 Ui_MainWindow, QtBaseWindow = uic.loadUiType("main_window.ui")
 
@@ -10,6 +11,17 @@ class MainWindow(QtBaseWindow, Ui_MainWindow):
         super().__init__(parent)
         self.setupUi(self)
         self.add_league.clicked.connect(self.add_button_clicked)
+        self.delete_league.clicked.connect(self.delete_button_clicked)
+        self.action_open.triggered.connect(self.action_open_triggered)
+
+    def action_open_triggered(self):
+        mb = QMessageBox(QMessageBox.Icon.Critical, "Ouch", "Can't open files", QMessageBox.StandardButton.Ok)
+        mb.exec()
+
+    def delete_button_clicked(self):
+        dialog = QMessageBox(QMessageBox.Icon.Question, "Are you sure?", "Are you sure you want to delete League",
+                             QMessageBox.StandardButton.Ok | QMessageBox.StandardButton.Cancel)
+        dialog.exec()
 
     def add_button_clicked(self):
         self.league_list.addItem(self.league_name_add.text())
