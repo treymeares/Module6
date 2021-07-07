22cae4ceeeee60363c6872ffee7cc67776de4ac5
trme3 <treymeares@gmail.com>
2021-07-07 09:57:40 -0400

    Added setup.py and README.md files

---
+++ module6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Module6',
+    version='1.1',
+    packages=['tests'],
+    package_dir={'': 'Module6'},
+    url='',
+    license='',
+    author='Trey Meares',
+    author_email='trey.meares@me.com',
+    description='League Database Explorer'
+)
